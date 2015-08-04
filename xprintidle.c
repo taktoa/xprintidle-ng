2b6d491dc3e9ba4790123d4ecc2d5ce619d488ef
Remy Goldschmidt <taktoa@gmail.com>
2015-08-04 15:02:14 -0500

    Clean up xprintidle.c, fix problem with memset

--- xprintidle.c
+++ xprintidle.c
@@ -1,32 +1,33 @@
 /*
 
-This program prints the "idle time" of the user to stdout.  The "idle
-time" is the number of milliseconds since input was received on any
-input device.  If unsuccessful, the program prints a message to stderr
-and exits with a non-zero exit code.
+  This program prints the "idle time" of the user to stdout.  The "idle
+  time" is the number of milliseconds since input was received on any
+  input device.  If unsuccessful, the program prints a message to stderr
+  and exits with a non-zero exit code.
 
-Copyright (c) 2005, 2008 Magnus Henoch <henoch@dtek.chalmers.se>
-Copyright (c) 2006, 2007 by Danny Kukawka
-                         <dkukawka@suse.de>, <danny.kukawka@web.de>
-Copyright (c) 2008 Eivind Magnus Hvidevold <hvidevold@gmail.com>
+  Copyright (c) 2005, 2008 Magnus Henoch <henoch@dtek.chalmers.se>
+  Copyright (c) 2006, 2007 by Danny Kukawka
+  <dkukawka@suse.de>, <danny.kukawka@web.de>
+  Copyright (c) 2008 Eivind Magnus Hvidevold <hvidevold@gmail.com>
+  Copyright (c) 2015 Remy Goldschmidt <taktoa@gmail.com>
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of version 2 of the GNU General Public License
-as published by the Free Software Foundation.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the
-Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the
+  Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-The function workaroundCreepyXServer was adapted from kpowersave-0.7.3 by
-Eivind Magnus Hvidevold <hvidevold@gmail.com>. kpowersave is licensed under
-the GNU GPL, version 2 _only_.
+  The function workaroundCreepyXServer was adapted from kpowersave-0.7.3 by
+  Eivind Magnus Hvidevold <hvidevold@gmail.com>. kpowersave is licensed under
+  the GNU GPL, version 2 _only_.
 
 */
 
@@ -35,80 +36,82 @@ the GNU GPL, version 2 _only_.
 #include <X11/extensions/scrnsaver.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <signal.h>
 #include <time.h>
 
 void usage(char *name);
-unsigned long workaroundCreepyXServer(Display *dpy, unsigned long _idleTime );
+unsigned long workaroundCreepyXServer(Display *dpy, unsigned long _idleTime);
 static void signal_callback_handler(int sig, siginfo_t *siginfo, void *context);
 
 Display *dpy;
 
-int main(int argc, char *argv[])
-{
-  XScreenSaverInfo ssi;
-//  Display *dpy;
-  int event_basep, error_basep;
+int main(int argc, char *argv[]) {
+    XScreenSaverInfo ssi;
+    //  Display *dpy;
+    int event_basep, error_basep;
 
-  if (argc != 1) {
-    usage(argv[0]);
-    return 1;
-  }
-  
-  dpy = XOpenDisplay(NULL);
-  if (dpy == NULL) {
-    fprintf(stderr, "couldn't open display\n");
-    return 1;
-  }
-  
-  struct sigaction act;
-  memset (&act, '\0', sizeof(act));
- 
-  /* Use the sa_sigaction field because the handles has two additional parameters */
-  act.sa_sigaction = &signal_callback_handler;
-		 
-  /* The SA_SIGINFO flag tells sigaction() to use the sa_sigaction field, not sa_handler. */
-  act.sa_flags = SA_SIGINFO;
-			 
-  // Register signal and signal handler
-  if (sigaction(SIGTERM, &act, NULL) < 0) {
-    perror ("sigaction");
-    return 1;
-  }
-
-  setlinebuf(stdout);
-
-  while(1) {
-	  
-    if (!XScreenSaverQueryExtension(dpy, &event_basep, &error_basep)) {
-      fprintf(stderr, "screen saver extension not supported\n");
-      return 1;
-    }
-  
-    if (!XScreenSaverQueryInfo(dpy, DefaultRootWindow(dpy), &ssi)) {
-      fprintf(stderr, "couldn't query screen saver info\n");
-      return 1;
+    if(argc != 1) {
+        usage(argv[0]);
+        return 1;
     }
 
-    printf("%lu\t%lu\n", time(NULL), workaroundCreepyXServer(dpy, ssi.idle));
-    sleep(1);
-  }
-  
-  return 0;
+    dpy = XOpenDisplay(NULL);
+    if(dpy == NULL) {
+        fprintf(stderr, "couldn't open display\n");
+        return 1;
+    }
+
+    struct sigaction act;
+    memset(&act, '\0', sizeof(act));
+
+    // Use the sa_sigaction field because the handles
+    // have two additional parameters
+    act.sa_sigaction = &signal_callback_handler;
+
+    // The SA_SIGINFO flag tells sigaction() to use the
+    // sa_sigaction field, not sa_handler.
+    act.sa_flags = SA_SIGINFO;
+
+    // Register signal and signal handler
+    if(sigaction(SIGTERM, &act, NULL) < 0) {
+        perror ("sigaction");
+        return 1;
+    }
+
+    setlinebuf(stdout);
+
+    while(1) {
+        if(!XScreenSaverQueryExtension(dpy, &event_basep, &error_basep)) {
+            fprintf(stderr, "screen saver extension not supported\n");
+            return 1;
+        }
+
+        if(!XScreenSaverQueryInfo(dpy, DefaultRootWindow(dpy), &ssi)) {
+            fprintf(stderr, "couldn't query screen saver info\n");
+            return 1;
+        }
+
+        printf("%lu\t%lu\n", time(NULL), workaroundCreepyXServer(dpy, ssi.idle));
+        sleep(1);
+    }
+
+    return 0;
 }
 
-static void signal_callback_handler(int sig, siginfo_t *siginfo, void *context) {
-  XCloseDisplay(dpy);
+static void signal_callback_handler(int sig,
+                                    siginfo_t *siginfo,
+                                    void *context) {
+    XCloseDisplay(dpy);
 }
 
-void usage(char *name)
-{
-  fprintf(stderr,
-	  "Usage:\n"
-	  "%s\n"
-	  "That is, no command line arguments.  The user's idle time\n"
-	  "in milliseconds is printed on stdout.\n",
-	  name);
+void usage(char *name) {
+    fprintf(stderr,
+            "Usage:\n"
+            "%s\n"
+            "That is, no command line arguments.  The user's idle time\n"
+            "in milliseconds is printed on stdout.\n",
+            name);
 }
 
 /*!
@@ -120,47 +123,51 @@ void usage(char *name)
  * This result in SUSE bug # and sf.net bug #. The bug in the XServer itself
  * is reported at https://bugs.freedesktop.org/buglist.cgi?quicksearch=6439.
  *
- * Workaround: Check if if XServer is in a dpms state, check the 
- *             current timeout for this state and add this value to 
+ * Workaround: Check if if XServer is in a dpms state, check the
+ *             current timeout for this state and add this value to
  *             the current idle time and return.
  *
  * \param _idleTime a unsigned long value with the current idletime from
  *                  XScreenSaverInfo->idle
  * \return a unsigned long with the corrected idletime
  */
-unsigned long workaroundCreepyXServer(Display *dpy, unsigned long _idleTime ){
-  int dummy;
-  CARD16 standby, suspend, off;
-  CARD16 state;
-  BOOL onoff;
+unsigned long workaroundCreepyXServer(Display *dpy, unsigned long _idleTime) {
+    int dummy;
+    CARD16 standby, suspend, off;
+    CARD16 state;
+    BOOL onoff;
 
-  if (DPMSQueryExtension(dpy, &dummy, &dummy)) {
-    if (DPMSCapable(dpy)) {
-      DPMSGetTimeouts(dpy, &standby, &suspend, &off);
-      DPMSInfo(dpy, &state, &onoff);
+    if(DPMSQueryExtension(dpy, &dummy, &dummy)) {
+        if(DPMSCapable(dpy)) {
+            DPMSGetTimeouts(dpy, &standby, &suspend, &off);
+            DPMSInfo(dpy, &state, &onoff);
 
-      if (onoff) {
-        switch (state) {
-          case DPMSModeStandby:
-            /* this check is a littlebit paranoid, but be sure */
-            if (_idleTime < (unsigned) (standby * 1000))
-              _idleTime += (standby * 1000);
-            break;
-          case DPMSModeSuspend:
-            if (_idleTime < (unsigned) ((suspend + standby) * 1000))
-              _idleTime += ((suspend + standby) * 1000);
-            break;
-          case DPMSModeOff:
-            if (_idleTime < (unsigned) ((off + suspend + standby) * 1000))
-              _idleTime += ((off + suspend + standby) * 1000);
-            break;
-          case DPMSModeOn:
-          default:
-            break;
+            if(onoff) {
+                switch(state) {
+                case DPMSModeStandby:
+                    // this check is a little bit paranoid, but just to be sure
+                    if(_idleTime < (unsigned) (standby * 1000)) {
+                        _idleTime += (standby * 1000);
+                    }
+                    break;
+                case DPMSModeSuspend:
+                    if(_idleTime < (unsigned) ((suspend + standby) * 1000)) {
+                        _idleTime += ((suspend + standby) * 1000);
+                    }
+                    break;
+                case DPMSModeOff:
+                    if(_idleTime < (unsigned) ((off + suspend + standby) * 1000)) {
+                        _idleTime += ((off + suspend + standby) * 1000);
+                    }
+                    break;
+                case DPMSModeOn:
+                    break;
+                default:
+                    break;
+                }
+            }
         }
-      }
-    } 
-  }
+    }
 
-  return _idleTime;
+    return _idleTime;
 }
